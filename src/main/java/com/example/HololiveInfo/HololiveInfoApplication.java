501c33587eabe3c22959d48f69d34ca3037c21ed
TK108 <rlaxorud0108@naver.com>
2024-07-20 13:48:06 +0900

    first commit

---
+++ src/main/java/com/example/HololiveInfo/HololiveInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HololiveInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HololiveInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HololiveInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HololiveInfo/HololiveInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HololiveInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HololiveInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
